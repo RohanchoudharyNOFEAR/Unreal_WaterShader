0bf9ca7b33f58336f0bee10e3785de43b20c0231
Rohan Choudhary <73844717+RohanchoudharyNOFEAR@users.noreply.github.com>
2026-02-08 10:16:40 +0530

    - folder structure setup

--- UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
+++ UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Game Development/Projects/Unreal Projects/UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
+#include "D:/Game Development/Projects/Unreal Projects/Unreal_WaterShader/UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
--- UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++ UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "D:/Game Development/Projects/Unreal Projects/UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "D:/Game Development/Projects/Unreal Projects/Unreal_WaterShader/UE_Assignment_Rohan/Intermediate/Build/Win64/x64/UE_Assignment_RohanEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
 #include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
